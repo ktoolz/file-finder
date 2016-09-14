4ca9ddd600fd64664f9fe29e6c83cd5b619a6051
Jean-Marc Desprez <jmdesprez@protonmail.com>
2016-09-14 14:06:54 +0200

    move search to controller + first implementation of bang and directories filters

--- core/src/main/kotlin/com/github/ktoolz/model/namespace.kt
+++ core/src/main/kotlin/com/github/ktoolz/model/namespace.kt
@@ -1,5 +1,6 @@
 package com.github.ktoolz.model
 
+import com.github.ktoolz.matchers
 import javaslang.collection.List
 import java.io.File
 
@@ -16,6 +17,33 @@ data class SearchResult(val matchers: List<PatternMatcher<Char>>, val file: File
 
 data class FilterQuery(val name: String, val negated: Boolean = false)
 
-data class SearchQuery(val term: String, val contexts: List<FilterQuery>, val directories: List<String>)
+data class SearchQuery(val term: String, val contexts: List<FilterQuery>, val directories: List<String>) {
+    fun filter(searchResult: SearchResult): Boolean {
+        val directoriesCheck = directories.forAll { searchResult.file.parentFile.absolutePath?.contains("/$it") ?: false }
 
-class PatternMatcher<T>(val matcher: T, val found: Boolean, val distance: Int)
+        if(contexts.isEmpty.not() && contexts.head().name == "src") {
+            if(searchResult.file.name.endsWith(".kt")) {
+                return !contexts.head().negated
+            } else return contexts.head().negated
+        }
+
+        return directoriesCheck
+    }
+}
+
+class PatternMatcher<out T>(val matcher: T, val found: Boolean, val distance: Int)
+
+fun Iterable<File>.search(searchQuery: SearchQuery) =
+        map { file ->
+            val lowerCaseName = file.name.toLowerCase()
+            SearchResult(lowerCaseName.matchers(searchQuery.term.toLowerCase()), file)
+        }
+                .apply {
+                    this.forEach {
+                        kotlin.io.println(it.file.toString())
+                        it.matchers.forEach { kotlin.io.println(it.matcher + " > " + it.found + " > " + it.distance) }
+                        kotlin.io.println("-----------")
+                    }
+                }
+                .filter { searchQuery.filter(it) }
+                .sortedBy { javaslang.Tuple3(-it.score, it.filename.length, it.filename) }
--- ui/src/main/kotlin/com/github/ktoolz/view/MainView.kt
+++ ui/src/main/kotlin/com/github/ktoolz/view/MainView.kt
@@ -1,15 +1,16 @@
 package com.github.ktoolz.view
 
 import com.github.ktoolz.controller.load
-import com.github.ktoolz.matchers
+import com.github.ktoolz.model.SearchQuery
 import com.github.ktoolz.model.SearchResult
+import com.github.ktoolz.model.search
+import com.github.ktoolz.parser.ContextParser
 import com.sun.javafx.collections.ObservableListWrapper
 import javafx.beans.property.SimpleStringProperty
 import javafx.scene.control.TableView
 import javafx.scene.control.TextField
 import javafx.scene.input.KeyCode.*
 import javafx.scene.layout.BorderPane
-import javaslang.Tuple3
 import tornadofx.*
 import java.io.File
 
@@ -51,7 +52,7 @@ class MainView : View() {
                 null -> result.clear()
                 else -> with(result) {
                     clear()
-                    addAll(search(searchText))
+                    addAll(search(ContextParser(javaslang.collection.List.of("src")).parse(searchText)))
                 }
             }
         }
@@ -109,36 +110,7 @@ class MainView : View() {
 
     }
 
-    private fun search(searchText: String): List<SearchResult> {
-        val lowerCaseSearch = searchText.toLowerCase()
-
-        // Exemple of ponderation, to be remove
-        // TODO remove
-        /*        val ponderation: (SearchResult) -> SearchResult = {
-                    when {
-                        it.filename.endsWith(".kt") -> it.copy(score = it.score + 2)
-                        it.filename.endsWith(".md") -> it.copy(score = it.score + 1)
-                        it.filename.endsWith(".class") -> it.copy(score = Int.MIN_VALUE)
-                        else -> it
-                    }
-                }*/
-        val toSearchResult: (File) -> SearchResult = { file ->
-            val lowerCaseName = file.name.toLowerCase()
-            SearchResult(lowerCaseName.matchers(lowerCaseSearch), file)
-        }
-        return files.map(toSearchResult)
-                //                .map(ponderation)
-                .apply {
-                    this.forEach {
-                        println(it.file.toString())
-                        it.matchers.forEach { println(it.matcher + " > " + it.found + " > " + it.distance) }
-                        println("-----------")
-                    }
-                }
-                .filter { it.score >= minScore }
-                .sortedBy { Tuple3(-it.score, it.filename.length, it.filename) }
-                .subList(0, 15)
-    }
+    private fun search(query: SearchQuery) = files.search(query).take(15)
 
 
     /**
