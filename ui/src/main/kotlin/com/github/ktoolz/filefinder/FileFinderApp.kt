3e0fa7c6b2226be1524ac271f652a542db0b8d4a
[a149812] Jean-Marc Desprez <jean-marc.desprez@atos.net>
2017-08-18 13:53:01 +0200

    Add parameter parsing

--- ui/src/main/kotlin/com/github/ktoolz/filefinder/FileFinderApp.kt
+++ ui/src/main/kotlin/com/github/ktoolz/filefinder/FileFinderApp.kt
@@ -19,6 +19,36 @@ import javafx.stage.Stage
 import tornadofx.App
 import tornadofx.importStylesheet
 
+sealed class Condition : (Application.Parameters) -> Boolean {
+    class HaveFlagPresent(val flag: String) : Condition() {
+        override fun invoke(parameters: Application.Parameters): Boolean =
+                parameters.unnamed.contains(flag)
+    }
+
+    class AnyFlagPresent(vararg val flag: String) : Condition() {
+        override fun invoke(parameters: Application.Parameters): Boolean =
+                flag.any { parameters.unnamed.contains(it) }
+
+        infix fun or(other: String) = AnyFlagPresent(*flag, other)
+    }
+
+    class AllFlagPresent(vararg val flag: String) : Condition() {
+        override fun invoke(parameters: Application.Parameters): Boolean =
+                flag.all { parameters.unnamed.contains(it) }
+
+        infix fun and(other: String) = AllFlagPresent(*flag, other)
+    }
+
+    class ParameterValue(val name: String, vararg val possibleValues: String) : Condition() {
+        override fun invoke(parameters: Application.Parameters): Boolean =
+                possibleValues.any { parameters.named[name] == it }
+    }
+}
+
+infix fun Application.Parameters.contains(condition: Condition) = condition(this)
+infix fun String.or(other: String) = Condition.AnyFlagPresent(this, other)
+infix fun String.and(other: String) = Condition.AllFlagPresent(this, other)
+
 /**
  * Main application.
  * Allows to start the GUI.
@@ -28,8 +58,18 @@ class FileFinderApp : App(MainView::class) {
         importStylesheet(Styles::class)
     }
 
+    private fun whenFlag(vararg flag: String, block: () -> Unit) {
+        if (flag.any { parameters.unnamed.contains(it) }) block()
+    }
+
+
     override fun start(stage: Stage) {
-        ExecutionContext.directories.addAll(parameters.unnamed.toFiles())
+
+        if(parameters contains ("-x" or "--exclusive")) {
+            ExecutionContext.directories.clear()
+        }
+
+        ExecutionContext.directories.addAll(parameters.unnamed.filter { !it.startsWith("-") }.toFiles())
         super.start(stage)
     }
 }
